10248f23954b878cb712844123f13931b8119276
alfiahrusli <62023253+alfiahrusli@users.noreply.github.com>
2020-05-05 04:36:42 -0700

    Add files via upload

---
+++ varStatic.java
@@ -0,0 +1,11 @@
+package no2;
+
+public class varStatic {
+    public static void main(String[] args){
+        String firstName = "Resky";
+        String lastName = "Ayu";
+        String FULLname = firstName + lastName;
+        
+        System.out.println(FULLname);
+    }
+}
